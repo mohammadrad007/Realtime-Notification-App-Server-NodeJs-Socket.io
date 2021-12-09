7b84950a3c216e2b9a498eaddb95943ba538401f
mohammad mohammadi <mohammad.mohamadi0101@gmail.com>
2021-12-09 22:32:44 +0330

    send-and-get-notif

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
-import { Server, Socket } from "socket.io";
+import { Server } from "socket.io";
 
 const io = new Server({
   cors: { origin: "http://localhost:3000" },
 });
 
-io.on("connection", (seocket) => {
-  console.log(Socket);
+let onlineUsers = [];
+
+const addNewUser = (username, socketId) => {
+  !onlineUsers.some((user) => user.username === username) &&
+    onlineUsers.push({ username, socketId });
+};
+
+const removeUser = (socketId) => {
+  onlineUsers = onlineUsers.filter((user) => user.socketId !== socketId);
+};
+
+const getUser = (username) => {
+  return onlineUsers.find((user) => user.username === username);
+};
+
+io.on("connection", (socket) => {
+  socket.on("newUser", (username) => {
+    addNewUser(username, socket.id);
+  });
+
+  socket.on("sendNotification", ({ senderName, receiverName, type }) => {
+    const receiver = getUser(receiverName);
+    io.to(receiver?.socketId).emit("getNotification", {
+      senderName,
+      type,
+    });
+  });
+
+  socket.on("disconnect", () => {
+    removeUser(socket.id);
+  });
 });
 
-io.listen(3000);
+io.listen(5000);
